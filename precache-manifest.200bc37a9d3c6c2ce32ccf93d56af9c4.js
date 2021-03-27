7c7587607cf120129869a1ec3e1f0f02b1b75389
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-27 23:36:44 +0200

    Updates

---
+++ precache-manifest.200bc37a9d3c6c2ce32ccf93d56af9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa4875d0c868b1537336c427bed7cf0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be681a8b873c4f52137a",
+    "url": "./static/css/main.23793ba8.chunk.css"
+  },
+  {
+    "revision": "205453e9777d64e69b03",
+    "url": "./static/js/2.bd866163.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.bd866163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be681a8b873c4f52137a",
+    "url": "./static/js/main.01a772fa.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
